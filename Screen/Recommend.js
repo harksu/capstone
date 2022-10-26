80ca912e2852e041146f0a3a947c4ea28de73352
harksu <harksulim@naver.com>
2022-10-26 16:44:19 +0900

    feat : menulistframe, todo : click event

--- Screen/Recommend.js
+++ Screen/Recommend.js
@@ -1,8 +1,37 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 const Recommend = () => {
+  const [isShow, setIsShow] = useState(true);
+  const [symptom, setSymptom] = useState("증상을 선택해주세요");
+
+  const onPress = () => {
+    setIsShow(!isShow);
+  };
+
+  const onPressSymptom = (item, index) => {
+    setSymptom(symptom.concat(item));
+  };
+
+  const templist = [
+    //이건 나중에 서버랑 이야기해서 맞추고
+    "감기입니다",
+    "가암기입니다",
+    "가아악기입니다",
+    "까마가윕니다.",
+    "까마가윕니다.",
+    "까마가윕니다.",
+    "까마가윕니다.",
+  ];
+
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
@@ -10,17 +39,33 @@ const Recommend = () => {
         <View style={styles.selectContainer}>
           <Text style={styles.text}>증상</Text>
           <View style={styles.dropDown}>
-            <Text style={styles.selectText}>증상을 선택해주세요</Text>
-            <TouchableOpacity style={styles.drop}>
+            <Text style={styles.selectText}>{symptom}</Text>
+            <TouchableOpacity onPressIn={onPress}>
               <Image source={require("../assets/drop.png")} />
             </TouchableOpacity>
           </View>
         </View>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.selectButton}>
-            <Text style={styles.buttonText}>확인</Text>
-          </TouchableOpacity>
-        </View>
+        {isShow ? (
+          <View style={styles.menuContainer}>
+            <View style={styles.menuItemBox}>
+              <ScrollView>
+                {templist?.map((item, index) => {
+                  return (
+                    <TouchableOpacity style={styles.menuItem} key={index}>
+                      <Text style={styles.menuText}>{item}</Text>
+                    </TouchableOpacity>
+                  );
+                })}
+              </ScrollView>
+            </View>
+          </View>
+        ) : (
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.selectButton}>
+              <Text style={styles.buttonText}>확인</Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
       <Footer />
     </View>
@@ -44,7 +89,7 @@ const styles = StyleSheet.create({
     marginTop: 90,
     marginLeft: "auto",
     marginRight: "auto",
-    //backgroundColor: "pink",
+    // backgroundColor: "pink",
     justifyContent: "space-between",
   },
   buttonContainer: {
@@ -55,10 +100,23 @@ const styles = StyleSheet.create({
     marginRight: "auto",
     marginTop: 20,
   },
+  menuContainer: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 20,
+    width: "90%",
+    height: 290,
+    //backgroundColor: "pink",
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "#a4ccff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   dropDown: {
-    alignSelf: "flex-end",
+    alignSelf: "center", //이건 원래 end였는데 왜 이렇게 했떠라
     justifyContent: "space-between",
-    width: "96%",
+    width: "100%", //이건 원래 96프로였음, 그러니까 밑에 마진이 4프로였겠지 근데 왜 ..?
     height: 50,
     borderRadius: 20,
     borderWidth: 2,
@@ -67,8 +125,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 10,
   },
+  menuItemBox: {
+    // backgroundColor: "pink",
+    width: "88%",
+    height: "90%",
+  },
+  menuItem: {
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: "#a4ccff",
+    opacity: 0.5,
+    height: 40,
+    marginBottom: 20, //이게 얼만지 안나옴
+    justifyContent: "center",
+    padding: 5,
+    backgroundColor: "#a4ccff",
+  },
   text: {
-    marginLeft: "4%",
+    //marginLeft: "2%", // 내가 이거 왜 넣었더라?(원래는 4프로긴한데)
     fontSize: 20,
     color: "#0974fa",
     lineHeight: 26,
@@ -86,6 +160,12 @@ const styles = StyleSheet.create({
     lineHeight: 26,
     fontFamily: "Tmoney",
   },
+  menuText: {
+    fontSize: 20,
+    lineHeight: 26,
+    fontFamily: "Tmoney",
+    marginLeft: 10,
+  },
   selectButton: {
     width: "25%",
     height: "100%",
@@ -97,4 +177,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  scroll: {},
 });
